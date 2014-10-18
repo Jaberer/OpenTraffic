c7da8ff0ecc8eb04a76a98f1bb4503610b7b956d
Ryan Senanayake <rsenapps@gmail.com>
2014-10-17 21:00:16 -0700

    work

--- SecondRoute/app/src/main/java/rsjz/com/secondroute/MainActivity.java
+++ SecondRoute/app/src/main/java/rsjz/com/secondroute/MainActivity.java
@@ -1,9 +1,14 @@
 package rsjz.com.secondroute;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +17,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        findViewById(R.id.set_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SetAddressActivity.class);
+                i.putExtra("home", true);
+                startActivity(i);
+
+            }
+        });
+        findViewById(R.id.set_work).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SetAddressActivity.class);
+                i.putExtra("home", false);
+                startActivity(i);
+            }
+        });
+        ((TextView)findViewById(R.id.home_address)).setText("Home Address: " + prefs.getString());
     }
 
 
